3d73f156e3c180f5fda19422cedd7dc0a28ce1de
m27cossi <moniquecossi2@gmail.com>
2025-10-08 21:34:30 -0300

    Aula 01

---
+++ JovemProgramadoraJava/src/jovemprogramadorajava/JovemProgramadoraJava.java
@@ -0,0 +1,35 @@
+
+package jovemprogramadorajava;
+
+
+public class JovemProgramadoraJava {
+
+   
+    public static void main(String[] args) {
+   
+   String nome = "Monique", sobrenome = "Cossi";     
+   
+   System.out.println("Eu me chamo "+ nome);
+   System.out.println(nome);
+        System.out.println("Ola mundo");
+    System.out.println(5);
+    System.err.println("love");
+    
+    int idade = 17 , anoNascimento =2008;
+    
+    double altura = 1.75;
+            boolean temCNH = false;
+            char sexo = 'F';
+  System.out.println("Eu me chamo " + nome + " " + sobrenome + ", " + "e tenho " + idade + " anos.");
+           
+  int idadeVelho;
+  idadeVelho = idade + 29;
+ int idadeAtual = 2025 - anoNascimento;
+ System.out.println( "voce tem " + idadeAtual +" anos");
+  
+System.out.println("Voce tera "+ idadeVelho + " anos");
+  
+
+
+   }
+}
